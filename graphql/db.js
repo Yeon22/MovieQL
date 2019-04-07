cdec1b5e67555a2866734b70134afb3b547c99ca
Suyeon <bsuyeon16@gmail.com>
2019-04-07 16:32:21 +0900

    create Mutation and moveis data

--- graphql/db.js
+++ graphql/db.js
@@ -19,7 +19,52 @@ export const people = [
   },
 ];
 
+const movies = [
+  {
+    id: 0,
+    name: "Star Wars - The new one",
+    score: 1,
+  },
+  {
+    id: 1,
+    name: "Avengers - End Game",
+    score: 8,
+  },
+  {
+    id: 2,
+    name: "The Godfather I",
+    score: 99,
+  },
+  {
+    id: 3,
+    name: "Logan",
+    score: 2,
+  },
+];
+
+export const getMovies = () => movies;
+
 export const getById = id => {
-  const filteredPeople = people.filter(person => id === person.id);
-  return filteredPeople[0];
+  const filteredMovies = movies.filter(movie => id === movie.id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanMovies = movies.filter(movie => movie.id !== id);
+  if (movies.length > cleanMovies.length) {
+    movies = cleanMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: movies.length + 1,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
 }
