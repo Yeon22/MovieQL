752f59e84d5fb64ce90799f999c77de6482d89a7
Suyeon Bak <bsuyeon16@gmail.com>
2021-02-14 18:31:57 +0900

    change base_url

--- graphql/db.js
+++ graphql/db.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const BASE_URL = "https://yts.am/api/v2/";
+const BASE_URL = "http://yts.am/api/v2/";
 const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
 const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
 const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
